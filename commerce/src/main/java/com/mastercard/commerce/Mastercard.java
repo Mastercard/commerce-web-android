1c8d875ce26e73d6d72b008c411001d1af316b85
e049652 <e049652@mastercard.com>
2020-02-24 16:49:07 -0500

    round checkout amount

--- commerce/src/main/java/com/mastercard/commerce/Mastercard.java
+++ commerce/src/main/java/com/mastercard/commerce/Mastercard.java
@@ -52,7 +52,7 @@ public class Mastercard extends CryptoOptions {
   }
 
   public Mastercard(@NonNull Set<MastercardFormat> formats) {
-    Validate.notEmpty(formats);
+    //Validate.notEmpty(formats);
     this.format = new ArrayList<>();
     for (MastercardFormat mastercardFormat : formats) {
       format.add(mastercardFormat.toString());
