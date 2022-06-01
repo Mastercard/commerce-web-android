f66a46d23204d36ff5c84e7826e162a400789d13
jkosha <nikita.koselev@gmail.com>
2022-06-01 22:50:28 +0100

    Revert "Update: Setting java environment on gradlew"
    This reverts commit edb2f77f5d9861e7bef5dc50b5ac6005594f7479.

--- gradlew.bat
+++ gradlew.bat
@@ -16,9 +16,6 @@ set APP_HOME=%DIRNAME%
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS=
 
-SET "JAVA_HOME=D:\java\zulu1.8.0_265\jre"
-echo Set Java Home to JAVA_HOME
-
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
 
